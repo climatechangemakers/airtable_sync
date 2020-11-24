80a894e3a23059d2f413b34de7060b3f1917e02b
Paul Cothenet <paul@madkudu.com>
2020-11-23 20:57:24 -0800

    Add function for finding active users

--- src/slack_to_airtable/active.js
+++ src/slack_to_airtable/active.js
@@ -5,61 +5,49 @@ const {
 const moment = require('moment');
 const throat = require('throat');
 const _ = require('lodash');
-const teams = require('./teams');
 
 const slack = new WebClient(process.env.SLACK_TOKEN);
 
 const base = require('airtable').base(process.env.AIRTABLE_BASE);
 
-// async function getSlackUsers() {
-//   const { members } = await slack.users.list();
-//   return parseUserResponse(members);
-// }
-
-// function parseUserResponse(members) {
-//   const shortList = members.map(member => {
-//     const { id, profile, name, updated, is_bot } = member;
-//     const { email, display_name, real_name } = profile;
-//     const updatedDate = moment.unix(updated).format('YYYY-MM-DD');
-//     return !is_bot && { id, email, name, real_name, display_name, updatedDate }
-//   });
-//   return _.compact(shortList);
-// }
-
-async function updateAirtable(slackUsers) {
-  base('CRM').select({
-    // maxRecords: 3,
-    view: "Joined Slack"
-  }).eachPage(async function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-    await Promise.all(records.map(throat(1, async (record) => {
-      // console.log(record);
-      const memberId = record.get('Slack Member ID');
-      const lastActiveRecord = record.get('Slack Last Active Date');
-      const lastActiveDate = lastActiveRecord ? moment(lastActiveRecord) : undefined;
-      if (!lastActiveDate || lastActiveDate < moment().startOf('day')) {
-        const { presence } = await slack.users.getPresence({ user: memberId });
-        console.log(memberId, lastActiveDate.toDate(), presence);
-        if (presence === 'active') {
-          await record.updateFields({
-            'Slack Last Active Date': moment().format('YYYY-MM-DD'),
-            'Last updated by Bot': moment().format('YYYY-MM-DD')
-          });
+async function updateAirtable() {
+  return new Promise((resolve, reject) => {
+    console.log(process.env.AIRTABLE_BASE);
+    base('CRM').select({
+      // maxRecords: 3,
+      view: "Joined Slack"
+    }).eachPage(async function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+      await Promise.all(records.map(throat(1, async (record) => {
+        const memberId = record.get('Slack Member ID');
+        const lastActiveRecord = record.get('Slack Last Active Date');
+        const lastActiveDate = lastActiveRecord ? moment(lastActiveRecord) : undefined;
+        // console.log(memberId, lastActiveDate);
+        if (!lastActiveDate || lastActiveDate < moment().startOf('day')) {
+          const { presence } = await slack.users.getPresence({ user: memberId });
+          // console.log(memberId, lastActiveDate.toDate(), presence);
+          if (presence === 'active') {
+            await record.updateFields({
+              'Slack Last Active Date': moment().format('YYYY-MM-DD'),
+              'Last updated by Bot': moment().format('YYYY-MM-DD')
+            });
+          }
+          await new Promise(resolve => setTimeout(resolve, 300)); // wait a bit for rate limit
         }
-        await new Promise(resolve => setTimeout(resolve, 500)); // wait a bit for rate limit
-      }
-    })));
-    fetchNextPage();
+      })));
+      fetchNextPage();
 
-  }, function done(err) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
+    }, function done(err) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve();
+    });
+  })
 }
 
 exports.handler = async function(_event, context) {
+  console.log('start');
   try {
     await updateAirtable();
   } catch (err) {
--- src/slack_to_airtable/sync.js
+++ src/slack_to_airtable/sync.js
@@ -5,7 +5,6 @@ const {
 const moment = require('moment');
 const throat = require('throat');
 const _ = require('lodash');
-const teams = require('./teams');
 
 const slack = new WebClient(process.env.SLACK_TOKEN);
 
@@ -27,54 +26,56 @@ function parseUserResponse(members) {
 }
 
 async function updateAirtable(slackUsers) {
-  base('CRM').select({
-    // maxRecords: 100,
-    view: "Grid view"
-  }).eachPage(async function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-    await Promise.all(records.map(throat(1, async (record) => {
-      // console.log(record);
-      const email = record.get('Email');
-      const name = `${record.get('First Name')} ${record.get('Last Name')}`
-      const slackJoinedDate = record.get('Slack Joined Date');
-      // console.log(email,name)
-      const slackMatch = slackUsers.find(slackUser => {
-        if (!slackUser.email) { return; }
-        return slackUser.email.toLowerCase() === email.toLowerCase().trim()
-          || (slackUser.real_name && slackUser.real_name.toLowerCase()) === name.toLowerCase();
-      });
-      if (slackMatch && !slackJoinedDate) {
-        console.log(email, slackJoinedDate, slackMatch);
-        try {
-          await record.updateFields({
-            'Slack Joined Date': slackMatch.updatedDate,
-            'Slack Member ID': slackMatch.id,
-            'Slack': 'Joined',
-            'Last updated by Bot': moment().format('YYYY-MM-DD')
-          })
-        } catch (err) {
-          console.log(err);
+  return new Promise((resolve, reject) => {
+    base('CRM').select({
+      // maxRecords: 100,
+      view: "Grid view"
+    }).eachPage(async function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+      await Promise.all(records.map(throat(1, async (record) => {
+        // console.log(record);
+        const email = record.get('Email');
+        const name = `${record.get('First Name')} ${record.get('Last Name')}`
+        const slackJoinedDate = record.get('Slack Joined Date');
+        // console.log(email,name)
+        const slackMatch = slackUsers.find(slackUser => {
+          if (!slackUser.email) { return; }
+          return slackUser.email.toLowerCase() === email.toLowerCase().trim()
+            || (slackUser.real_name && slackUser.real_name.toLowerCase()) === name.toLowerCase();
+        });
+        if (slackMatch && !slackJoinedDate) {
+          console.log(email, slackJoinedDate, slackMatch);
+          try {
+            await record.updateFields({
+              'Slack Joined Date': slackMatch.updatedDate,
+              'Slack Member ID': slackMatch.id,
+              'Slack': 'Joined',
+              'Last updated by Bot': moment().format('YYYY-MM-DD')
+            })
+          } catch (err) {
+            console.log(err);
+          }
         }
+      })));
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+
+    }, function done(err) {
+      if (err) {
+        return reject(err);
       }
-    })));
-
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-  }, function done(err) {
-    if (err) {
-      console.error(err);
-      return;
-    }
+      return resolve()
+    });
   });
 }
 
 exports.handler = async function(_event, context) {
   try {
     const slackUsers = await getSlackUsers();
-    console.log(slackUsers);
+    // console.log(slackUsers);
     await updateAirtable(slackUsers);
   } catch (err) {
     console.log(err);
